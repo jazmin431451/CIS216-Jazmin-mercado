f42f12a6a20187b20196c5d13f271ac830f01d11
jazmin <97916000+jazmin431451@users.noreply.github.com>
2023-10-20 21:53:59 -0500

    Add files via upload

--- assignment_8/CardGameApp.py
+++ assignment_8/CardGameApp.py
@@ -1,28 +1,5 @@
 import tkinter as tk
-import random
 from deck import Deck
-from card import Card
-from CardGameApp import CardGameApp
-
-class Card:
-    def __init__(self, suit, rank):
-        self.suit = suit
-        self.rank = rank
-
-    def __str__(self):
-        return f"{self.rank} of {self.suit}"
-
-class Deck:
-    def __init__(self):
-        suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
-        ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
-        self.cards = [Card(suit, rank) for suit in suits for rank in ranks]
-        random.shuffle(self.cards)
-
-    def draw(self):
-        if self.cards:
-            return self.cards.pop(0)
-        return None
 
 class CardGameApp:
     def __init__(self, root):
@@ -100,8 +77,3 @@ class CardGameApp:
         self.player1_card = None
         self.player2_card = None
         self.update_labels()
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = CardGameApp(root)
-    root.mainloop()
