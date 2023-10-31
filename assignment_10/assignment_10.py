04caff5699a223dddb92688eb18a520c7b16a8e8
jazmin <97916000+jazmin431451@users.noreply.github.com>
2023-10-31 13:31:16 -0500

    Add files via upload

--- assignment_10/assignment_10.py
+++ assignment_10/assignment_10.py
@@ -1,76 +1,93 @@
 import tkinter as tk
-from tkinter import filedialog, colorchooser, messagebox
+from tkinter import font
+from tkinter import colorchooser
 
-def open_file():
-    file_path = filedialog.askopenfilename(filetypes=[("Text Files", "*.txt")])
-    if file_path:
-        with open(file_path, 'r') as file:
-            text.delete(1.0, tk.END)
-            text.insert(tk.END, file.read())
+current_file = None  # To track the currently open file
+modified = False  # To track if the text has been modified
 
-def save_file():
-    file_path = filedialog.asksaveasfilename(filetypes=[("Text Files", "*.txt")])
-    if file_path:
-        with open(file_path, 'w') as file:
-            file.write(text.get(1.0, tk.END))
+def open_file(event=None):
+    global current_file, modified
+    file_path = tk.filedialog.askopenfilename(filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
+    if not file_path:
+        return
+    current_file = file_path
+    with open(file_path, "r") as file:
+        text = file.read()
+        txt_edit.delete(1.0, tk.END)
+        txt_edit.insert(tk.END, text)
+    window.title(f"Text Editor - {current_file}")
+    modified = False
 
-def save_file_as():
-    save_file()
+def save_file(event=None):
+    global current_file, modified
+    if current_file:
+        with open(current_file, "w") as file:
+            text = txt_edit.get(1.0, tk.END)
+            file.write(text)
+        modified = False
 
-def change_format():
+def change_font():
+    font_obj = font.nametofont(txt_edit.cget("font"))
+    font = tk.font.Font(root=txt_edit, family=font_obj.actual("family"), size=12)
+    txt_edit.tag_configure("custom_font", font=font)
+    txt_edit.tag_add("custom_font", txt_edit.index(tk.SEL_FIRST), txt_edit.index(tk.SEL_LAST))
+
+def change_text_color():
     color = colorchooser.askcolor()[1]
-    text.configure(fg=color)
+    if color:
+        txt_edit.tag_configure("custom_color", foreground=color)
+        txt_edit.tag_add("custom_color", txt_edit.index(tk.SEL_FIRST), txt_edit.index(tk.SEL_LAST))
 
-def show_custom_dialog():
-    custom_dialog = tk.Toplevel(root)
-    custom_dialog.title("Custom Dialog")
+def save_file_as(event=None):
+    global current_file, modified
+    file_path = tk.filedialog.asksaveasfilename(defaultextension="txt", filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
+    if not file_path:
+        return
+    current_file = file_path
+    with open(file_path, "w") as file:
+        text = txt_edit.get(1.0, tk.END)
+        file.write(text)
+    window.title(f"Text Editor - {current_file}")
+    modified = False
 
-    label = tk.Label(custom_dialog, text="This is a custom dialog box.")
-    label.pack()
+def text_modified(event=None):
+    global modified
+    modified = True
 
-    ok_button = tk.Button(custom_dialog, text="OK", command=custom_dialog.destroy)
-    ok_button.pack()
+window = tk.Tk()
+window.title("Text Editor")
 
-def show_info_message():
-    messagebox.showinfo("Information", "This is an information message.")
+window.rowconfigure(0, weight=1)
+window.columnconfigure(1, weight=1)
 
-def show_error_message():
-    messagebox.showerror("Error", "This is an error message.")
+txt_edit = tk.Text(window)
+txt_edit.grid(row=0, column=1, sticky="nsew")
+txt_edit.bind("<Key>", text_modified)
 
-def ask_question():
-    result = messagebox.askquestion("Question", "Do you want to continue?")
-    if result == "yes":
-        print("User wants to continue.")
-    else:
-        print("User doesn't want to continue.")
+scrollbar = tk.Scrollbar(window, command=txt_edit.yview)
+scrollbar.grid(row=0, column=2, sticky="ns")
+txt_edit.config(yscrollcommand=scrollbar.set)
 
-root = tk.Tk()
-root.title("Text Editor")
+menu_bar = tk.Menu(window)
+window.config(menu=menu_bar)
 
-menu = tk.Menu(root)
-root.config(menu=menu)
+file_menu = tk.Menu(menu_bar)
+menu_bar.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="Open", command=open_file, accelerator="Ctrl+O")
+file_menu.add_command(label="Save", command=save_file, accelerator="Ctrl+S")
+file_menu.add_command(label="Save As...", command=save_file_as, accelerator="Ctrl+Shift+S")
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=window.quit)
 
-file_menu = tk.Menu(menu)
-menu.add_cascade(label="File", menu=file_menu)
-file_menu.add_command(label="Open", command=open_file)
-file_menu.add_command(label="Save", command=save_file)
-file_menu.add_command(label="Save As", command=save_file_as)
+format_menu = tk.Menu(menu_bar)
+menu_bar.add_cascade(label="Format", menu=format_menu)
+format_menu.add_command(label="Change Font", command=change_font)
+format_menu.add_command(label="Change Text Color", command=change_text_color)
 
-format_menu = tk.Menu(menu)
-menu.add_cascade(label="Format", menu=format_menu)
-format_menu.add_command(label="Change Color", command=change_format)
+window.bind("<Control-o>", open_file)
+window.bind("<Control-s>", save_file)
+window.bind("<Control-S>", save_file_as)
 
-custom_menu = tk.Menu(menu)
-menu.add_cascade(label="Custom", menu=custom_menu)
-custom_menu.add_command(label="Show Custom Dialog", command=show_custom_dialog)
+window.protocol("WM_DELETE_WINDOW", window.quit)
 
-message_menu = tk.Menu(menu)
-menu.add_cascade(label="Messages", menu=message_menu)
-message_menu.add_command(label="Show Info Message", command=show_info_message)
-message_menu.add_command(label="Show Error Message", command=show_error_message)
-message_menu.add_command(label="Ask Question", command=ask_question)
-
-text = tk.Text(root)
-text.pack()
-
-root.mainloop()
+window.mainloop()
